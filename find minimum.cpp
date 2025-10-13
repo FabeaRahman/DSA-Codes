f64c5ad9350e103c4bc1b728df852e209fb0363c
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-10-13 21:53:17 +0600

    Add files via upload

---
+++ find minimum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a,b;
+    cin>>a>>b;
+    if(a<b){
+        cout<<"Smaller number is:"<<a<<endl;
+    }
+    else
+    cout<<"Smaller number is:"<<b<<endl;
+    return 0;
+}
