44526ea3ffb4dc823cf50642a69c1c281fe72fbc
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-10-13 22:52:15 +0600

    Add files via upload

---
+++ code for input and print of a array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int i,n,sum=0,count=0;
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];///input
+
+        }
+        for(i=0; i<n; i++)
+        {
+            cout<<arr[i]<<' ' ;///print the array
+        }
+  
+return 0;
+}
