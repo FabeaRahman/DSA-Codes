a02742e73b3d9792b406b53230671439fdd4190d
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-10-13 22:39:48 +0600

    Add files via upload

---
+++ count the value in array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+
+    int i,n,count=0;
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];///input
+
+        }
+
+for(i=0; i<n; i++){
+            if(arr[i]>40)
+                    {count ++;}
+}
+            cout<<count<<endl;
+            /// count the number
+        
+        return 0;}
