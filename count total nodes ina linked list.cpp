0560a88920a2d5f5d046e6981ea7c7a4374b1480
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-11-08 23:16:50 +0600

    Add files via upload

---
+++ count total nodes ina linked list.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int countNodes(Node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+int main() {
+    Node* head = new Node{10, new Node{20, new Node{30, NULL}}};
+
+    cout << "Total Nodes = " << countNodes(head);
+    return 0;
+}
